671bdb6aaba333a7767bfbae74fc335ff0a0652f
Wef2 <qhrud5725@hanmail.net>
2016-05-27 18:27:01 +0900

    Initialize Project

---
+++ src/main/java/kr/ac/jejunu/Greeting.java
@@ -0,0 +1,23 @@
+package kr.ac.jejunu;
+
+/**
+ * Created by Kim on 2016-05-27.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
