97ad3aa3bfc03a3cd506cecf247ab20aaafafc42
Wef2 <qhrud5725@hanmail.net>
2016-06-07 16:41:08 +0900

    Add Webjar

---
+++ src/main/java/kr/ac/jejunu/PageController.java
@@ -0,0 +1,42 @@
+package kr.ac.jejunu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by neo-202 on 2016-06-07.
+ */
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/board")
+    public String board(){
+        return "board";
+    }
+
+    @RequestMapping("/modification")
+    public String modification(){
+        return "modification";
+    }
+
+    @RequestMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+
+
+
+
+}
