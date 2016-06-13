1bc889d97af785dd900b57a291394fe066c8c230
Wef2 <qhrud5725@hanmail.net>
2016-06-13 18:11:51 +0900

    Add Interceptor

---
+++ src/main/java/kr/ac/jejunu/AppConfiguration.java
@@ -0,0 +1,21 @@
+package kr.ac.jejunu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Kim on 2016-06-13.
+ */
+
+@Configuration
+@ComponentScan("kr.ac.jejunu")
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor());
+    }
+}
---
+++ src/main/java/kr/ac/jejunu/MyInterceptor.java
@@ -0,0 +1,24 @@
+package kr.ac.jejunu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Kim on 2016-06-13.
+ */
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+    private final static Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info(request.getRequestURI());
+        return true;
+    }
+
+}
