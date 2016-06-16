a00540d15a4bb78aa60c9b1c88e492ce275fd331
Wef2 <qhrud5725@hanmail.net>
2016-06-17 02:46:51 +0900

    Update Ajax

--- src/main/resources/static/js/home_app.js
+++ src/main/resources/static/js/home_app.js
@@ -1,14 +1,54 @@
 $(document).ready(function () {
 
-    $.ajax({
-        url: "/rest/articles/page/1"
-    }).then(function (data) {
-        $.each(data.content, function(i, item) {
-            $('.body-articles').append(
-                "<div class='row'>" + item.id + "</div>"
-            );
+    getPage(0);
+
+    var totalPages;
+    var isFirst;
+    var isLast;
+
+    function getPage(pageNumber){
+        $.ajax({
+            url: "/rest/articles/page/" + pageNumber
+        }).then(function (data) {
+            totalPages = data.totalPages;
+            isFirst = data.first;
+            isLast = data.last;
+
+            console.log(totalPages);
+            console.log(isFirst);
+            console.log(isLast);
+            $.each(data.content, function (i, item) {
+                $('.body-articles').append(
+                    "<tr class='row'>" +
+                    "<td class='col-lg-2 text-center'>" + "<img class='profile-image' src='/user/" + item.userId + "/profile'/> </td>" +
+                    "<td class='col-lg-2'>" + item.userId + "</td>" +
+                    "<td class='col-lg-3'>" + item.content + "</td>" +
+                    "<td class='col-lg-3 row'>"
+                    + "<form method='post'>"
+                    + "<input type='submit' class='btn btn-primary col-lg-4'>"
+                    + "</form>"
+                    + "<form method='post'>"
+                    + "<input type='submit' class='btn btn-danger col-lg-4'>"
+                    + "</form>"
+                    + "<form method='post'>"
+                    + "<input type='submit' class='btn btn-info col-lg-4'>"
+                    + "</form>" +
+                    "</td>" +
+                    "<td class='col-lg-2 article-content'>" + item.date + "</td>" +
+                    "</tr>"
+                );
+            });
+
+            var i = 0;
+            while(i < totalPages){
+                $('.paging-btn-group').append(
+                    "<button class='btn btn-default'>" + i + "</button>"
+                );
+                i = i + 1;
+            }
             console.log(item);
         });
-    });
+    }
+
 
 });
